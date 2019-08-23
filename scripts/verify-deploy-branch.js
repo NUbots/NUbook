47e2492c06d218db30788c07cff161bd374d8e44
Josephus Paye II <j.paye96@gmail.com>
2019-08-23 13:01:59 +1000

    Disable deployments from a non-master branch

---
+++ scripts/verify-deploy-branch.js
@@ -0,0 +1,8 @@
+const { execSync } = require('child_process')
+
+const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+
+if (branch !== 'master') {
+  console.error(`Cannot deploy from a non-master branch. Current branch is ${branch}.`)
+  process.exit(1)
+}
