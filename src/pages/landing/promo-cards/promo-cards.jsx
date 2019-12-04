394664c15ad8a6ce178a040ca1e8687df9970db0
Josephus Paye II <j.paye96@gmail.com>
2019-12-04 22:37:22 +1100

    Simplify promo cards with CSS grid

--- src/pages/landing/promo-cards/promo-cards.jsx
+++ src/pages/landing/promo-cards/promo-cards.jsx
@@ -4,10 +4,11 @@ import IconBall from './icon-ball.svg'
 import IconBrain from './icon-brain.svg'
 import IconHandshake from './icon-handshake.svg'
 import PromoCard from './promo-card'
+import style from './promo-cards.module.css'
 
 const PromoCards = () => {
   return (
-    <div className='max-w-screen-xl mx-auto px-6 flex flex-wrap justify-center lg:flex-no-wrap mb-12'>
+    <div className={`${style.cards} max-w-screen-xl mx-auto px-6 mb-24`}>
       <PromoCard Icon={IconBrain} title='Robotics Research'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ut
         placeat quos doloribus iure blanditiis voluptas perspiciatis earum
