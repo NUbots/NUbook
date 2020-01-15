2569c1b05ad6ff91e35b49af631e42d401572fe9
Josephus Paye II <j.paye96@gmail.com>
2020-01-15 21:48:49 +1100

    Update comments and whitespace in setup script

--- src/book/02-system/01-nubots/assets/ci_setup.sh
+++ src/book/02-system/01-nubots/assets/ci_setup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-BUILDKITE_AGENT_TOKEN="replace-this"
-DOCKER_LOGIN_PASSWORD="replace-this"
+BUILDKITE_AGENT_TOKEN="<INSERT AGENT TOKEN HERE>"
+DOCKER_LOGIN_PASSWORD="<INSERT DOCKERHUB ACCOUNT PASSWORD HERE>"
 
+# Print the credentials for reference
 echo BUILDKITE_AGENT_TOKEN=${BUILDKITE_AGENT_TOKEN}
 echo DOCKER_LOGIN_PASSWORD=${DOCKER_LOGIN_PASSWORD}
 
 # Remove old Docker
 apt-get remove docker docker-engine docker.io containerd runc
+
 # Install packages to allow apt to use a repository over HTTPS
 apt-get update
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
 # Add Docker's stable repository
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 ​
@@ -19,28 +23,32 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bi
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 
-# Create a 'docker' user group and add local users
-groupadd docker                    # Create user group
-usermod -aG docker nubots          # Add nubots user to group
-usermod -aG docker buildkite-agent # Add buildkite-agent to group
-newgrp docker                           # Activate group changes
-# Configure Docker to start on boot
+# Create a 'docker' user group and add local users to allow for use without sudo
+sudo groupadd docker                     # Create user group
+sudo usermod -aG docker nubots           # Add nubots to the group
+sudo usermod -aG docker buildkite-agent  # Add buildkite-agent to the group
+newgrp docker                            # Activate group changes
+
+# Configure Docker to run on system startup
 systemctl enable docker
 
-# Add the signed buildkite apt repository
+# Add the signed Buildkite apt repository
 echo "deb https://apt.buildkite.com/buildkite-agent stable main" | tee /etc/apt/sources.list.d/buildkite-agent.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 32A37959C2FA5C3C99EFBC32A79206696452D198
-# Install the agent
+
+# Install the Buildkite agent
 apt-get update
 apt-get install -y buildkite-agent
-# Configure the agent token
-sed -i "s/xxx/${BUILDKITE_AGENT_TOKEN}/g" /etc/buildkite-agent/buildkite-agent.cfg
-# Start the agent
-systemctl enable buildkite-agent &&   systemctl start buildkite-agent
 
-# Make the docker login password an environment variable
-echo -e "set -e\n export DOCKER_LOGIN_PASSWORD=\"${DOCKER_LOGIN_PASSWORD}\""  > /etc/buildkite-agent/hooks/environment
+# Add the agent token to the configuration file
+sed -i "s/xxx/${BUILDKITE_AGENT_TOKEN}/g" /etc/buildkite-agent/buildkite-agent.cfg
+
+# Start the agent and schedule it to run on system startup
+systemctl enable buildkite-agent && systemctl start buildkite-agent
+
+# Make the DockerHub login password available to agents via an environment variable
+echo -e "set -e\n export DOCKER_LOGIN_PASSWORD=\"${DOCKER_LOGIN_PASSWORD}\"" > /etc/buildkite-agent/hooks/environment
 chmod +x /etc/buildkite-agent/hooks/environment
 
-# Set the dockerhub tag to true
+# Set `dockerhub` tag on the agent to indicate that it has the login credentials
 sed -i "$a tags=\"dockerhub=true\"" /etc/buildkite-agent/buildkite-agent.cfg
