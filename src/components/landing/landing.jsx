f291b727439e4cc716472af5fbd3e57bb23f4cb4
Josephus Paye II <j.paye96@gmail.com>
2020-06-15 22:36:52 +1000

    Update landing blurb

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -75,9 +75,7 @@ const LandingPage = props => {
         <div className='relative text-center mb-20'>
           <h2 className='text-4xl mb-8'>Welcome to NUbook</h2>
           <div className='text-secondary text-lg max-w-3xl mx-auto'>
-            The NUbots team documentation. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Molestiae minima ipsa dolor dolorum
-            perferendis labore eum tempora quia commodi eveniet.
+            NUbook is the NUbots team documentation and handbook. It contains information about the team, our areas of research, publications, and team history; a description of our hardware, software systems, and related tools; as well as guides on contributing to NUbots.
           </div>
         </div>
 
