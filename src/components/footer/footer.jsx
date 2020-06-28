8734c38a13904ee941bc51c73e96e18bb71c8ba6
Ysobel <ysobelsims@gmail.com>
2020-06-28 15:48:45 +1000

    Add netlify badge to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,6 +10,14 @@ const Footer = () => (
   <div className='max-w-screen-xl mx-auto px-8'>
     <div className='pt-12 pb-16 border-t border-gray-200 text-gray-500 text-center flex flex-col lg:flex-row lg:text-left'>
       <div className='mb-6 lg:mb-0 lg:w-1/3'>
+        <a href='https://www.netlify.com'>
+          <img
+            className='opacity-50 hover:opacity-100 pb-6'
+            src='https://www.netlify.com/img/global/badges/netlify-dark.svg'
+            alt='Deploys by Netlify'
+            title='Netlify'
+          />
+        </a>
         Copyright © {new Date().getFullYear()} NUbots -{' '}
         <a
           className='hover:text-gray-800'
