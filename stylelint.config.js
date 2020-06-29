784255066e142dad83f551cd6a0261a7c89aab17
Josephus Paye II <j.paye96@gmail.com>
2020-06-29 18:45:37 +1000

    Add stylelint, fix codebase

---
+++ stylelint.config.js
@@ -0,0 +1,28 @@
+/*eslint-env node*/
+
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'no-descending-specificity': null,
+    'declaration-empty-line-before': 'never',
+    'declaration-block-no-duplicate-properties': true,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+  },
+}
