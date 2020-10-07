9560399c1330771ce6fab30bea3687e33d024eab
Moosa Hassan <moosa7021@gmail.com>
2020-10-07 14:12:34 +1100

    Referencing System
    <references/> tag reads .bib

---
+++ src/components/markdown/references/references.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
+
+import { BibReferencesContext } from '../../page-template'
+
+const References = ({ children }) => {
+  const { references, usedReferences, addUsedReference } = useContext(
+    BibReferencesContext
+  )
+
+  const referenceId = String(children)
+
+  // looping array
+  const referenceOptions = Array.from(usedReferences).map((referenceId, i) => {
+    const fields = references[referenceId].fields
+    // fields now has all the data you need, e.g. fields.author, fields.title
+
+    console.log('REFERENCE ID: ' + referenceId)
+
+    // returning values
+    return (
+      <div id={`#reference-${referenceId}`}>
+        <p>
+          {' '}
+          {i + 1}. {fields.author} ({fields.year}) - <i>{fields.title}</i> -{' '}
+          <a target='_blank' href={fields.url}>
+            {fields.url}
+          </a>
+        </p>
+      </div>
+    )
+  })
+
+  // return the map
+  return <div>{referenceOptions}</div>
+}
+
+References.PropTypes = {
+  children: PropTypes.isRequired,
+}
+
+export default References
