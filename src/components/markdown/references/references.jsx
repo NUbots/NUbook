b124ccba0516dbbad708609215fb44f6c0c72b75
Moosa Hassan <moosa7021@gmail.com>
2020-10-07 13:16:27 +1100

    referencing is done (needs different styling)

--- src/components/markdown/references/references.jsx
+++ src/components/markdown/references/references.jsx
@@ -3,14 +3,38 @@ import PropTypes from 'prop-types'
 
 import { BibReferencesContext } from '../../page-template'
 
-const References = ({ children }) => {  
-    return (
-      <p>the references component works</p>
+const References = ({ children }) => {
+  const { references, usedReferences, addUsedReference } = useContext(
+    BibReferencesContext
+  )
+  
+  const referenceId = String(children)
+
+  // looping array
+  const referenceOptions = Array.from(usedReferences).map((referenceId, i) => {
+    const fields = references[referenceId].fields
+    // fields now has all the data you need, e.g. fields.author, fields.title
+
+    // returning values
+    return(
+      <div>
+        <p> {i+1}. {fields.author} ({fields.year}) - <i>{fields.title}</i> - <a target="_blank" href={fields.url} >{fields.url}</a></p>
+        
+      </div>
     )
+  });
+
+  // return the map
+  return(
+    <div>
+      {referenceOptions}
+    </div>
+  )
+
 }
 
 References.PropTypes = {
-    children: PropTypes.node,
+  children: PropTypes.isRequired,
 }
 
 export default References
