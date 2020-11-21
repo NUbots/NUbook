75e133f9f0ec8c41ed5c216add660054d2ab0b0d
Moosa Hassan <moosahassan73@gmail.com>
2020-11-21 16:02:25 +1100

    different citation styles and entries

--- src/components/markdown/references/references.jsx
+++ src/components/markdown/references/references.jsx
@@ -1,34 +1,206 @@
 import React, { useContext } from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, { array } from 'prop-types'
 
 import { BibReferencesContext } from '../../page-template'
 
-const References = ({ children }) => {
+const References = ({ children, citationStyle }) => {
   const { references, usedReferences } = useContext(
     BibReferencesContext
   )
 
-  const referenceId = String(children)
 
   // looping array
   const referenceOptions = Array.from(usedReferences).map((referenceId, i) => {
-    const fields = references[referenceId].fields
-    // fields now has all the data you need, e.g. fields.author, fields.title
 
-    console.log('REFERENCE ID: ' + referenceId)
+    const fields = references[referenceId].fields     // fields handles all the data you need, e.g. author, title, year, etc
+    const types = references[referenceId].type        // types handles the type of bibtex reference entry
 
-    // returning values
-    return (
-      <div id={`#reference-${referenceId}`}>
-        <p>
-          {' '}
-          {i + 1}. {fields.author} ({fields.year}) - <i>{fields.title}</i> -{' '}
-          <a target='_blank' href={fields.url}>
-            {fields.url}
-          </a>
-        </p>
-      </div>
-    )
+    const authorName = fields.author.split(' ');    // splitting author name for future use
+
+    // APA STYLING
+    if(citationStyle == "APA")
+    {
+      // book
+      if(types == "book")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]}, {authorName[0].charAt(0)}.  ({fields.year}). <i>{fields.title}</i>. {fields.publisher}.
+            </p>
+          </div>
+        )
+      }
+      // article
+      else if(types == "article")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]}, {authorName[0].charAt(0)}. ({fields.year}). <i>{fields.title}</i>{' '}
+              <a target='_blank' href={fields.url}>
+                {fields.url}
+              </a>
+            </p>
+          </div>
+        )
+      } 
+    }
+
+    // MLA
+    else if(citationStyle == "MLA")
+    {
+      // book
+      if(types == "book")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]}, {authorName[0]}. <i>{fields.title}</i>. {fields.publisher}, {fields.year}.
+            </p>
+          </div>
+        )
+      }
+      // article
+      else if(types == "article")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]}, {authorName[0]}. "{fields.title}", {fields.year}, <a target='_blank' href={fields.url}>{fields.url}</a>
+            </p>
+          </div>
+        )
+      }
+    }
+
+    // Harvard
+    else if(citationStyle == "Harvard")
+    {
+      if(types == "book")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]}, {authorName[0].charAt(0)}., {fields.year}. <i>{fields.title}</i>. {fields.address}: {fields.publisher}.
+            </p>
+          </div>
+        )
+      }
+      else if(types == "article")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]}, {authorName[0].charAt(0)}., {fields.year}. <i>{fields.title}</i>. [online] Available at: {'<'}<a target='_blank' href={fields.url}>{fields.url}</a>{'>'}.
+            </p>
+          </div>
+        )
+      }
+    }
+
+    // Chicago
+    else if(citationStyle == "Chicago")
+    {
+      if(types == "book")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]}, {authorName[0]}. {fields.year}. <i>{fields.title}</i>. {fields.address}: {fields.publisher}.
+            </p>
+          </div>
+        )
+      }
+      else if(types == "article")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]}, {authorName[0]}. {fields.year}. "{fields.title}". <a target='_blank' href={fields.url}>{fields.url}</a>.
+            </p>
+          </div>
+        )
+      }
+    }
+
+    // IEEE
+    else if(citationStyle == "IEEE")
+    {
+      if(types == "book")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              [{i + 1}]{authorName[0].charAt(0)}. {authorName[1]}, <i>{fields.title}</i>. {fields.address}: {fields.publisher}, {fields.year}.
+            </p>
+          </div>
+        )
+      }
+      else if(types == "article")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              [{i + 1}]{authorName[0].charAt(0)}. {authorName[1]}, "{fields.title}", {fields.year}. [Online]. Available: <a target='_blank' href={fields.url}>{fields.url}</a>.
+            </p>
+          </div>
+        )
+      }
+    }
+
+    // AMA
+    else if(citationStyle == "AMA")
+    {
+      if(types == "book")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]} {authorName[0].charAt(0)}, <i>{fields.title}</i>. {fields.address}: {fields.publisher}, {fields.year}.
+            </p>
+          </div>
+        )
+      }
+      else if(types == "article")
+      {
+        return (
+          <div id={`#reference-${referenceId}`}>
+            <p>
+              {' '}
+              {i + 1}. {authorName[1]} {authorName[0].charAt(0)}. <a target='_blank' href={fields.url}>{fields.url}</a>. {fields.title}. Published {fields.year}.
+            </p>
+          </div>
+        )
+      }
+    }
+
+    // No citation style specified
+    else
+    {
+      return (
+        <div id={`#reference-${referenceId}`}>
+          <p>
+            {' '}
+            {i + 1}. {fields.author} ({fields.year}) - <i>{fields.title}</i> -{' '}
+            <a target='_blank' href={fields.url}>
+              {fields.url}
+            </a>
+          </p>
+        </div>
+      )
+    }
   })
 
   // return the map
