dec70bb024b1184b16ca6eb8010e0eff1e9fcf1e
Josephus Paye II <j.paye96@gmail.com>
2021-08-21 23:04:56 +1000

    Use W3C datetime format for dates in the sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,9 +150,76 @@ module.exports = {
         filterPages: () => true,
         serialize: (page) => ({
           url: page.url,
-          mod: page.lastModified,
+          lastmod: toW3CString(page.lastModified),
         }),
       },
     },
   ],
+}
+
+// Convert the given date string to a string in the W3C datetime format (yyyy-mm-ddThh:ii:ss+zz:zz).
+// Adapted from https://gist.github.com/tristanlins/6585391
+function toW3CString(dateString) {
+  const date = new Date(dateString)
+
+  const year = date.getFullYear()
+  let month = date.getMonth()
+  month++
+  if (month < 10) {
+    month = '0' + month
+  }
+  let day = date.getDate()
+  if (day < 10) {
+    day = '0' + day
+  }
+
+  let hours = date.getHours()
+  if (hours < 10) {
+    hours = '0' + hours
+  }
+
+  let minutes = date.getMinutes()
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+
+  let seconds = date.getSeconds()
+  if (seconds < 10) {
+    seconds = '0' + seconds
+  }
+
+  const offset = -date.getTimezoneOffset()
+  let offsetHours = Math.abs(Math.floor(offset / 60))
+  let offsetMinutes = Math.abs(offset) - offsetHours * 60
+
+  if (offsetHours < 10) {
+    offsetHours = '0' + offsetHours
+  }
+
+  if (offsetMinutes < 10) {
+    offsetMinutes = '0' + offsetMinutes
+  }
+
+  let offsetSign = '+'
+  if (offset < 0) {
+    offsetSign = '-'
+  }
+
+  return (
+    year +
+    '-' +
+    month +
+    '-' +
+    day +
+    'T' +
+    hours +
+    ':' +
+    minutes +
+    ':' +
+    seconds +
+    offsetSign +
+    offsetHours +
+    ':' +
+    offsetMinutes
+  )
 }
