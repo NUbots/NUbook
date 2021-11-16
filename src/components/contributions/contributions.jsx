e216f49ed55a5bf04c48285b9ceff8ef67fcf23a
Lachlan Johnson <lachlanm.johnson@gmail.com>
2021-11-16 13:17:06 +1100

    Add commit data to Contributions component

--- src/components/contributions/contributions.jsx
+++ src/components/contributions/contributions.jsx
@@ -1,26 +1,58 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import EditIcon from './edit-icon.svg'
 
-const lastUpdated = '11th Oct 2021'
-
-const Contributions = () => {
+const Contributions = ({ data }) => {
+  const commit = data.github.repository.object.history.nodes[0]
+  const file = data.github.repository.object.file
+  const lastUpdated = new Date(commit.author.date).toLocaleString('en-AU', {
+    dateStyle: 'long',
+  })
+  const editLink = `https://github.com/NUbots/NUbook/edit/main/${file.path}`
   return (
     <div className='text-secondary dark:text-secondary-inverted text-base mt-6 border-t-2 border-b-2 border-gray-200 dark:border-gray-800 py-3 flex'>
       <div className='flex-grow'></div>
       <div className='pr-3 capitalize'>
-        <a className='lg:normal-case'>
+        <a
+          href={commit.url}
+          target='_blank'
+          className='lg:normal-case'
+          rel='noreferrer'
+        >
           <span className='hidden lg:inline'>Last</span> updated{' '}
           <span className='text-nubots-700'>{lastUpdated}</span>
         </a>
       </div>
       <div className='border-l-2 dark:border-gray-800 pl-3'>
-        <button className='flex items-center w-full'>
+        <a href={editLink} className='flex items-center w-full'>
           <EditIcon className='h-5' />
           <span className='pl-1 hidden lg:block text-nubots-700'>Edit</span>
-        </button>
+        </a>
       </div>
     </div>
   )
+}
+
+Contributions.propTypes = {
+  data: PropTypes.shape({
+    github: PropTypes.shape({
+      repository: PropTypes.shape({
+        object: PropTypes.shape({
+          history: PropTypes.shape({
+            nodes: PropTypes.arrayOf({
+              author: PropTypes.shape({
+                date: PropTypes.string,
+              }),
+              url: PropTypes.string,
+            }),
+          }),
+          file: PropTypes.shape({
+            path: PropTypes.string,
+          }),
+        }),
+      }),
+    }),
+  }),
 }
 
 export default Contributions
