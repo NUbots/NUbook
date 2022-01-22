0e91b83ecb965787c31f94431180dc66b75d457f
Josephus Paye II <j.paye96@gmail.com>
2022-01-22 14:22:16 +1100

    Update the display components to show the new data

--- src/components/contributions/contributions.jsx
+++ src/components/contributions/contributions.jsx
@@ -2,42 +2,56 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import EditIcon from './edit-icon.svg'
 
-const Contributions = ({ date, url, mdxPath, contributors }) => {
-  const lastUpdated = new Date(date).toLocaleString('en-AU', {
-    dateStyle: 'medium',
-  })
+import NUbotsIcon from '../../images/nubots-icon.svg'
 
-  const editLink = `https://github.com/NUbots/NUbook/edit/main/${mdxPath}`
+const Contributions = ({ mdxPath, contributions }) => {
+  const { authors, lastCommit } = contributions
+
   return (
-    <div className='text-sm text-secondary dark:text-secondary-inverted mb-6 border-t-2 border-b-2 border-gray-200 dark:border-gray-800 py-2 flex items-center'>
-      <div className='flex-grow flex gap-x-1 flex-wrap h-7 overflow-hidden'>
-        {contributors.map((contributor, i) => {
-          return (
-            <a key={i} href={contributor.url}>
+    <div className='text-xs text-secondary dark:text-secondary-inverted mb-6 border-t border-b border-gray-200 dark:border-gray-800 py-2 flex items-center'>
+      <div className='flex-grow flex gap-x-1 flex-wrap'>
+        {authors.map((author) => {
+          return author.username ? (
+            <a
+              key={author.username ?? author.name}
+              href={`https://github.com/${author.username}`}
+              title={author.name}
+            >
               <img
-                className='h-7 rounded-full'
-                title={contributor.name}
-                src={contributor.avatar}
+                className='h-7 w-7 rounded-full'
+                src={`https://avatars.githubusercontent.com/${author.username}`}
+                alt={`${author.name} GitHub avatar`}
               />
             </a>
+          ) : (
+            <span className='inline-flex' title={author.name}>
+              <NUbotsIcon className='w-7 h-7 inline-flex' />
+            </span>
           )
         })}
       </div>
-      <div className='pr-3 capitalize inline-block whitespace-nowrap ml-1'>
-        <span className='hidden lg:inline'>Last</span> updated{' '}
-        <a
-          href={url}
-          target='_blank'
-          className='lg:normal-case'
-          rel='noreferrer'
-        >
-          <span className='text-nubots-700 dark:text-nubots-500'>
-            {lastUpdated}
-          </span>
-        </a>
-      </div>
+      {lastCommit && (
+        <div className='pr-3 inline-block whitespace-nowrap ml-1'>
+          <span className='md:hidden'>Updated </span>
+          <span className='hidden md:inline'>Last updated </span>
+          <a
+            href={`https://github.com/NUbots/NUbook/commit/${lastCommit.hash}`}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <span className='text-nubots-700 dark:text-nubots-500'>
+              {new Date(lastCommit.date).toLocaleString('en-AU', {
+                dateStyle: 'medium',
+              })}
+            </span>
+          </a>
+        </div>
+      )}
       <div className='border-l-2 dark:border-gray-800 pl-3'>
-        <a href={editLink} className='flex items-center w-full'>
+        <a
+          href={`https://github.com/NUbots/NUbook/edit/main/${mdxPath}`}
+          className='flex items-center w-full'
+        >
           <EditIcon className='h-5' />
           <span className='pl-1 hidden lg:block text-nubots-700 dark:text-nubots-500'>
             Edit
@@ -49,16 +63,19 @@ const Contributions = ({ date, url, mdxPath, contributors }) => {
 }
 
 Contributions.propTypes = {
-  date: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
   mdxPath: PropTypes.string.isRequired,
-  contributors: PropTypes.arrayOf(
-    PropTypes.shape({
-      avatar: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-    })
-  ).isRequired,
+  contributions: PropTypes.shape({
+    authors: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        username: PropTypes.string,
+      })
+    ).isRequired,
+    lastCommit: PropTypes.shape({
+      date: PropTypes.string.isRequired,
+      hash: PropTypes.string.isRequired,
+    }),
+  }),
 }
 
 export default Contributions
