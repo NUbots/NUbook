73560689959a66889c409ed9d2446552fa68fe9c
LachlanCourt <c3308061@uon.edu.au>
2022-06-01 11:24:38 +1000

    Move event listener back to static folder

--- src/components/header/search/algolia-search.js
+++ src/components/header/search/algolia-search.js
@@ -35,19 +35,3 @@ export const initialiseAlgolia = () => {
     })
   })
 }
-// Add support for the / keyboard shortcut
-window.addEventListener('keydown', (event) => {
-  // Don't do anything if an input is focused
-  if (
-    document.activeElement instanceof HTMLInputElement ||
-    document.activeElement instanceof HTMLTextAreaElement
-  ) {
-    return
-  }
-
-  if (event.key === '/') {
-    event.preventDefault()
-    const searchBox = document.querySelector('#search')
-    searchBox && searchBox.focus()
-  }
-})
