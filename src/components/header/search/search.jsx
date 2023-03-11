d1e31380c0ecced05bb832f4beba9721c5110f00
LachlanCourt <c3308061@uon.edu.au>
2023-03-11 15:30:28 +1100

    WIP

--- src/components/header/search/search.jsx
+++ src/components/header/search/search.jsx
@@ -6,11 +6,11 @@ import * as style from './search.module.css'
 
 import { initialiseAlgolia } from './algolia-search'
 
-const Search = ({ background }) => {
-  useEffect(() => {
-    initialiseAlgolia()
-  }, [])
+import { DocSearch } from '@docsearch/react'
 
+import '@docsearch/css'
+
+const Search = ({ background }) => {
   return (
     <div
       className={`${style.search} ${
@@ -19,16 +19,24 @@ const Search = ({ background }) => {
           : style.backgroundTransparent
       }`}
     >
-      <input
+      {/* <input
         id='search'
         placeholder='Search NUbook...'
         title='Keyboard shortcut: /'
         autoComplete='off'
         className={`${style.searchInput} h-10 w-full pl-10 md:pl-14 pr-3 md:pr-4 rounded appearance-none text-lg outline-none`}
-      />
-      <SearchIcon
+      /> */}
+      <div className={`${style.searchInput}`}>
+        <DocSearch
+          appId='BH4D9OD16A'
+          apiKey='be16e460d9d03fa711df82d525dec3c1'
+          indexName='nubots'
+          placeholder='Search NUbook...'
+        />
+        {/* <SearchIcon
         className={`${style.searchIcon} absolute top-0 left-0 w-5 md:w-6 h-6 ml-3 md:ml-4 mt-2 pointer-events-none`}
-      />
+      /> */}
+      </div>
     </div>
   )
 }
