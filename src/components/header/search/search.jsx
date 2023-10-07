a78aac7d39fc6e920c2e78e2191f86614c4ba914
LachlanCourt <c3308061@uon.edu.au>
2023-10-07 22:53:14 +1100

    Tidy up files

--- src/components/header/search/search.jsx
+++ src/components/header/search/search.jsx
@@ -1,27 +1,17 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-
-import SearchIcon from './search-icon.svg'
 import '@docsearch/css'
 import * as style from './search.module.css'
-
-import { initialiseAlgolia } from './algolia-search'
-
 import { DocSearch } from '@docsearch/react'
-
-
-
-const Search = ({ background }) => {
+const Search = () => {
   return (
-
-      <div className={`${style.search}`}>
-        <DocSearch
-          appId='BH4D9OD16A'
-          apiKey='be16e460d9d03fa711df82d525dec3c1'
-          indexName='nubots'
-          placeholder='Search NUbook...'
-        />
-       
+    <div className={`${style.search}`}>
+      <DocSearch
+        appId='BH4D9OD16A'
+        apiKey='be16e460d9d03fa711df82d525dec3c1'
+        indexName='nubots'
+        placeholder='Search NUbook...'
+      />
     </div>
   )
 }
@@ -35,24 +25,3 @@ Search.defaultProps = {
 }
 
 export default Search
-
-
-  //  {/*  <div
-  //     className={`${style.search} ${
-  //       background === 'solid'
-  //         ? style.backgroundSolid
-  //         : style.backgroundTransparent
-  //     }`}
-  //   >
-  //     <input
-  //       id='search'
-  //       placeholder='Search NUbook...'
-  //       title='Keyboard shortcut: /'
-  //       autoComplete='off'
-  //       className={`${style.searchInput} h-10 w-full pl-10 md:pl-14 pr-3 md:pr-4 rounded appearance-none text-lg outline-none`}
-  //     /> */}
-
-  //      {/* <SearchIcon
-  //       className={`${style.searchIcon} absolute top-0 left-0 w-5 md:w-6 h-6 ml-3 md:ml-4 mt-2 pointer-events-none`}
-  //     /> 
-  //     </div>*/}
