bcce838046a742fb0e4309a7e7817079654a7c8f
LachlanCourt <c3308061@uon.edu.au>
2023-10-10 17:47:17 +1100

    Apply other code suggestions from code review

--- src/components/header/search/search.jsx
+++ src/components/header/search/search.jsx
@@ -3,14 +3,27 @@ import PropTypes from 'prop-types'
 import '@docsearch/css'
 import * as style from './search.module.css'
 import { DocSearch } from '@docsearch/react'
+
 const Search = () => {
   return (
-    <div className={`${style.search}`}>
+    <div className={style.search}>
       <DocSearch
         appId='BH4D9OD16A'
         apiKey='be16e460d9d03fa711df82d525dec3c1'
         indexName='nubots'
         placeholder='Search NUbook...'
+        transformItems={(items) => {
+          return items.map((item) => {
+            // Get the origin (domain + port, if any) from the result URL
+            const { origin } = new URL(item.url)
+            return {
+              ...item,
+              // Replace the origin in the result URL with our current origin,
+              // so the links work for local development and deploy previews
+              url: item.url.replace(origin, window.location.origin),
+            }
+          })
+        }}
       />
     </div>
   )
