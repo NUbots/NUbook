b38064770020d070ca5266f318296b9202a79344
LachlanCourt <lachlan.m.court@gmail.com>
2024-02-17 12:05:41 +1100

    Remove unused variable

--- src/components/header/search/search.jsx
+++ src/components/header/search/search.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import '@docsearch/css'
-import * as style from './search.module.css'
+import './search.module.css'
 import { DocSearch } from '@docsearch/react'
 
 const Search = () => {
